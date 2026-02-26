9b126acfa65d558195ac94310c73d1ee2a92e1d3
Grace Brown <weevilwilson19@gmail.com>
2026-02-25 22:39:25 -0600

    New Files

---
+++ Labs/IncelRange.cpp
@@ -0,0 +1,59 @@
+/***************************
+* Name       : Grace Brown *
+* Last Edited: 26 Feb 2026 *
+* File Name  : Lab5.cpp    *
+* Purpose    :  		   *
+***************************/
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+using namespace std;
+
+int main(){
+	
+	/*CONCEPT : INCEL DUEL!
+		 You are at an anime convention
+		 You are wearing a poorly made cosplay of your favorite anime character
+		 You find another person ALSO wearing a cosplay of the SAME CHARACTER
+		 You have to fight -- NERD STYLE -- to determine who TRULY deserves to embody the character
+		 */
+		 
+	string name, cosplayChar, catchPhrase;
+	int playerHealth, enemyHealth, playerDamage, enemyDamage, menuChoice, rageMultiplier, defense, randChance;
+	char yesNo;
+	
+	srand(time); 
+	
+	//Ascii Art
+	//Story Setup
+	
+	//player info input and variable assignments
+	
+	//story continues -- enemy appears
+	
+	//RANDOMIZER FOR ENEMY STATS
+	//Enemy stats
+	
+	//DO WHILE GAME LOOP
+	//nested do while menu loop
+	//user validation
+	
+	/* CHOICES:
+		1.) Attack 
+		2.) Defend
+		3.) Rummage Bag  -- This one offers a randomizer for a boost
+		4.) Give up      -- Automatically sets player health to 0 */
+		
+	//user Validation loop for choices
+	
+	//Display results of choice
+	
+	// KEEP LOOPING WHILE(playerHealth > 0 || enemyHealth > 0)!!!
+	
+	//Ouside of Do While -- give final stats and complete storyline
+	
+	return;
+}
