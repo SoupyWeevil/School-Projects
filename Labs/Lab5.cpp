5515f35dd7d509d4a576697ba83287192fc70205
Grace Brown <weevilwilson19@gmail.com>
2026-03-01 15:30:26 -0600

    Updated lab5 +1 hour

--- Labs/Lab5.cpp
+++ Labs/Lab5.cpp
@@ -21,7 +21,7 @@ int main(){
 		 You have to fight -- NERD STYLE -- to determine who TRULY deserves to embody the character
 		 */
 		 
-	string name, cosplayChar, catchPhrase;
+	string name, cosplayChar, catchPhrase, border(50, '-'), prop;
 	int playerHealth, enemyHealth, playerDamage, enemyDamage, menuChoice, rageMultiplier, defense, randChance;
 	char yesNo;
 	
@@ -45,11 +45,168 @@ int main(){
  \____/\___/|_| |_|\__|_|  \___/ \_/ \___|_|  |___/\__, |
                                                     __/ |
                                                    |___/ 
-●・○・●・○・●・○・●・○・●・○・●・○・●・○・●・○・●・○・●・○・●・○
-	)";
+●・○・●・○・●・○・●・○・●・○・●・○・●・○・●・○・●・○・●・○・●・○)";
 
 	//Story Setup ... Madlibs that determine stats and name
+	cout << "\n" << border << endl 
+		 << "\nYou enter the convention lobby..."
+		 << "\nThere are booths beyond your scope of vision"
+		 << "\nCharacters posing for pictures at every corner"
+		 << "\nEverything is perfect..."
+		 << "\nUntil you see them.\n"
+		 << "\n" << border << "\n"
+		 << "\nBut first, what is your name? --> ";
+	getline(cin, name);
 	
+	do{ //Allows you to choose your name
+		cout << "\nAre you sure your name is " << name << " ?\n"
+			 << "[Y/N] --> ";
+		cin >> yesNo;
+		yesNo = toupper(yesNo);
+
+		while(cin.fail() || yesNo != 'Y' && yesNo != 'N'){ //User validation for name
+			cin.clear();
+			cin.ignore(100, '\n');
+			cout << "\nPlease Enter a Valid Choice! --> ";
+			cin >> yesNo;
+			yesNo = toupper(yesNo);
+		}
+		cin.ignore(100, '\n');
+
+		if(yesNo == 'N'){ //Chooses NEW name if you mistyped
+			cout << "\nPlease enter your REAL name --> ";
+			getline(cin, name);
+		}
+	}while(yesNo == 'N');
+
+	cout << "\nNow, " << name << ", who are you cosplaying as? --> ";
+	getline(cin, cosplayChar);
+
+	do{ //Allows you to choose your cosplay character
+		cout << "\n" << cosplayChar << "! Did I hear that right?\n"
+		 << "[Y/N] --> ";
+		cin >> yesNo;
+		yesNo = toupper(yesNo);
+
+		while(cin.fail() || yesNo != 'Y' && yesNo != 'N'){ //User validation for cosplay
+			cin.clear();
+			cin.ignore(100, '\n');
+			cout << "\nPlease Enter a Valid Choice! --> ";
+			cin >> yesNo;
+			yesNo = toupper(yesNo);
+		}
+		cin.ignore(100, '\n');
+
+		if(yesNo == 'N'){ //Chooses NEW cosplay if you mistyped
+			cout << "\nI'm sorry! Who are you cosplaying as then? --> ";
+			getline(cin, cosplayChar);
+		}
+	}while(yesNo == 'N');
+
+	cout << "\n" << cosplayChar << ", SURELY you have a prop? A weapon?\n"
+		 << "[Y/N] --> ";
+	cin >> yesNo;
+	yesNo = toupper(yesNo);
+
+	do{ //Allows you to choose or reject a weapon
+		while(cin.fail() || yesNo != 'Y' && yesNo != 'N'){ //User validation for weapon
+			cin.clear();
+			cin.ignore(100, '\n');
+			cout << "\nPlease Enter a Valid Choice! --> ";
+			cin >> yesNo;
+			yesNo = toupper(yesNo);
+		}
+		cin.ignore(100, '\n');
+
+		if(yesNo == 'Y'){ //Allows you to enter the name of your prop
+			cout << "\nWhat is your prop?\n"
+				 << "[Enter Name of Prop] --> ";
+			getline(cin, prop);
+
+			do{
+				cout << "\nYour prop is " << prop << ", did I hear that right?"
+					 << "\n[Y/N] --> ";
+				cin >> yesNo;
+				yesNo = toupper(yesNo);
+
+				while(cin.fail() || yesNo != 'Y' && yesNo != 'N'){ //User validation for weapon
+					cin.clear();
+					cin.ignore(100, '\n');
+					cout << "\nPlease Enter a Valid Choice! -->";
+					cin >> yesNo;
+					yesNo = toupper(yesNo);
+				}
+				cin.ignore(100, '\n');
+
+				if(yesNo == 'N'){
+					cout << "\nOh! I'm sorry, its very hard to hear in here!\n"
+						 << "What is your prop? --> ";
+					getline(cin, prop);
+				}
+				playerDamage = 20; // If player HAS a prop, attack stats are RAISED
+			}while(yesNo == 'N');
+		}else{
+			cout << "\n Are you sure you don't have a prop?\n"
+				 << "\n[Y/N] --> ";
+			cin >> yesNo;
+			yesNo = toupper(yesNo);
+
+			while(cin.fail() || yesNo != 'Y' && yesNo != 'N'){ //User validation for weapon
+					cin.clear();
+					cin.ignore(100, '\n');
+					cout << "\nPlease Enter a Valid Choice! -->";
+					cin >> yesNo;
+					yesNo = toupper(yesNo);
+				}
+			cin.ignore(100, '\n');
+
+			if(yesNo == 'Y'){
+				playerDamage = 10; //If player does NOT have a prop, attack stats stay neutral at 10
+			}else{
+				cout << "\nWhat is your prop?"
+					 << "\n[Enter Name of Prop] --> ";
+				getline(cin, prop);
+
+				do{
+					cout << "\nYour prop is " << prop << ", did I hear that right?"
+						 << "\n[Y/N] --> ";
+					cin >> yesNo;
+					yesNo = toupper(yesNo);
+
+					while(cin.fail() || yesNo != 'Y' && yesNo != 'N'){ //User validation for weapon
+						cin.clear();
+						cin.ignore(100, '\n');
+						cout << "\nPlease Enter a Valid Choice! -->";
+						cin >> yesNo;
+						yesNo = toupper(yesNo);
+					}
+					cin.ignore(100, '\n');
+
+					if(yesNo == 'N'){
+						cout << "\nOh! I'm sorry, its very hard to hear in here!\n"
+							 << "What is your prop? --> ";
+						getline(cin, prop);
+					}
+					playerDamage = 20; // If player HAS a prop, attack stats are RAISED
+				}while(yesNo == 'N');
+			}
+		}
+	}while(yesNo == 'N');
+
+	cout << "[PRESS ENTER TO CONTINUE STORY]";
+	cin.ignore(100, '\n');
+
+	cout << " " << border << endl
+		 << "\nAs you were walking along the convention floor..."
+		 << "\nYou see Them."
+		 << "\nYour doppleganger. "
+		 << "\nAnother " << cosplayChar << ". But their cosplay is significantly better than yours."
+		 << "\nYou cant let them mog you...\n"
+		 << border << endl
+		 << "\n[PRESS ENTER TO CONTINUE]";
+	cin.ignore(100, '\n');
+
+
 	//Player info and stats
 	
 	//story continues -- enemy appears
