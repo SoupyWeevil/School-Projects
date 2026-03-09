ebe0702f85d089f2243c6a038ac6001f9f3af714
Grace Brown <weevilwilson19@gmail.com>
2026-03-09 10:45:48 -0500

    created branch structure for main

--- Labs/Lab 6/Lab6.cpp
+++ Labs/Lab 6/Lab6.cpp
@@ -8,7 +8,25 @@
 #include "lab6.h"
 
 int main(){
-    
+    int choice; //holds return for displayMenu()
+    do{ //Begins "gameplay" loop
+        choice = displayMenu(); 
+
+        switch(choice){ //takes return from menu and splits into different case functions
+            case 1: //calls SQUARE
+
+                break;
+            case 2: //calls RECTANGLE
+
+                break;
+            case 3: //calls PARALLELOGRAM
+
+                break;
+            case 4: //calls CIRCLE
+            
+                break;
+        }
+    }while(choice != 5); //continues to cycle loop until EXIT is chosen
     return 0;
 }
 
