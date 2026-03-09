d56c36c9da8fdf0a8eed596334526f5e3710739b
Grace Brown <weevilwilson19@gmail.com>
2026-03-09 10:39:42 -0500

    Added descriptions for each function definition

--- Labs/Lab 6/functions.cpp
+++ Labs/Lab 6/functions.cpp
@@ -7,25 +7,85 @@
 
 #include "lab6.h"
 
-void displayMenu(){
+/*
+    Return Type : Integer
+    Name        : displayMenu
+    Parameters  : NONE
+    Use         : displays menu options and allows user choice
+    Return      : user choice
+*/
+int displayMenu(){
+    int choice;
+    cout << "\n┌──────────── ⋆⋅☆⋅⋆ ────────────┐"
+         << "\n|  Choose an Area to Calculate  |"
+         << "\n|-------------------------------|"
+         << "\n| 1.) Square                    |"
+         << "\n| 2.) Rectangle                 |"
+         << "\n| 3.) Parallelogram             |"
+         << "\n| 4.) Circle                    |"
+         << "\n| 4.) EXIT                      |"
+         << "\n└──────────── ⋆⋅☆⋅⋆ ────────────┘\n"
+         << "\nEnter Choice Here --> ";
+    cin >> choice;
 
+    while(cin.fail() || (choice > 5 || choice < 1)){
+        if(cin.fail()){
+            cout << "\nPlease enter an Integer! --> ";
+        }else{
+            cout << "\nPlease enter an integer 1-5! --> ";
+        }
+        cin.clear();
+        cin.ignore(100, '\n');
+
+        cin >> choice;
+    }
+
+    return choice;
 }
 
+/*
+    Return Type : Double
+    Name        : areaSquare
+    Parameters  : (double)side 
+    Use         : Accepts user input from main and calculates the area of the square based on the input
+    Return      : calculated solution for user's square
+*/
 double areaSquare(double side){
 
     return 0;
 } 
 
+/*
+    Return Type : Double
+    Name        : areaRectangle
+    Parameters  : (double)height, (double)width
+    Use         : Accepts user input from main and calculates area of rectangle based on input
+    Return      : calculated solution for user's rectangle
+*/
 double areaRectangle(double height, double width){
 
     return 0;
 }
 
+/*
+    Return Type : Double
+    Name        : areaParallelogram
+    Parameters  : (double)base, (double)height
+    Use         : Accepts user input from main and calculates araea based on input
+    Return      : calculated solution for user's parallelogram
+*/
 double areaParallelogram(double base, double height){
 
     return 0;
 }
 
+/*
+    Return Type : Double
+    Name        : areaCircle
+    Parameters  : (double)radius
+    Use         : Accepts user inptu from main and calculated area based on input AND global constant PI
+    Return      : calculated solution for user's rectangle
+*/
 double areaCircle(double radius){
 
     return 0;
--- Labs/Lab 6/lab6.h
+++ Labs/Lab 6/lab6.h
@@ -16,11 +16,11 @@ using namespace std;
 
 const double PI = 3.14159;
 
-void displayMenu(); // displays menu, allows user choice, RETURNS user choice
-double areaSquare(double side); 
-double areaRectangle(double height, double width);
-double areaParallelogram(double base, double height);
-double areaCircle(double radius);
+int displayMenu(); // displays menu, allows user choice, RETURNS user choice
+double areaSquare(double side);  //calculated area of square based on user input
+double areaRectangle(double height, double width); //calculates area of rectangle based on user input
+double areaParallelogram(double base, double height); //calculates area of parallelogram based on user input
+double areaCircle(double radius); //calculates area of circle based on user input
 
 
 #endif
